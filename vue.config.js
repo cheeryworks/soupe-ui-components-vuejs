1f05883ac419acae025a1b76e8f5566630a83de1
Carr Chang <carr.z.chang@outlook.com>
2020-08-18 20:16:47 +0800

    Fix docs site path error

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/soupe-ui-components-vuejs/' : '/',
+  publicPath: '',
   outputDir: 'docs',
   chainWebpack: (config) => {
     config.module
