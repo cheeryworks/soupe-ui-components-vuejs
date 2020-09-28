067d769e8471a4f4c28f265ef0ab6e9aa7402e2c
Carr Chang <carr.z.chang@outlook.com>
2020-09-28 11:44:04 +0800

    Add arrow icon to accordion title

--- src/components/accordion/AccordionItemTitle.vue
+++ src/components/accordion/AccordionItemTitle.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="hero-head soupe-ui-accordion-item-title px-3 py-3" :class="{'is-active': active}">
-    <div class="columns" v-if="title">
-      <div class="column" v-html="title"></div>
+    <div class="columns">
+      <div class="column">
+        <slot></slot>
+        <span v-if="title">{{ title }}</span>
+      </div>
       <div class="column is-narrow">
         <span
           class="tag is-rounded is-danger"
@@ -9,8 +12,12 @@
           v-if="alert || alert === 0"
         ></span>
       </div>
+      <div class="column is-narrow">
+        <span class="icon">
+          <i class="fas" :class="{'fa-angle-right': !active, 'fa-angle-down': active}"></i>
+        </span>
+      </div>
     </div>
-    <slot></slot>
   </div>
 </template>
 
